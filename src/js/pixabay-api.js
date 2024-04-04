13180ef82ff1cf87a44e5641d44d1ed2f5b095c0
george9907 <109024372+george9907@users.noreply.github.com>
2024-04-04 04:15:12 +0200

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,16 +4,16 @@ import { inputSearch, showLoader } from '../main';
 export function pixabayPhoto() {
     const formValue = inputSearch.value.trim().split(',').join('+');
     const searchParams = new URLSearchParams({
-        key: "42633759-4a44573e34755b4488adb4c1b",
+        key: "43219571-0a26e2304ee50d7f75a2b614a",
         q: [formValue],
         image_type: "photo",
         orientation: "horizontal",
         safesearch: true
     });
     showLoader();
-    return fetch(`https://pixabay.com/api/?${searchParams}`)
-        .then((response) => {
-            if (!response.ok) {
+    return fetch(`https://pixabay.com/api/?${searchParams}`).then(res => res.json())
+        .then((res) => {
+            if (!res.ok) {
                 throw new Error(response.status);
             }
             return response.json();
